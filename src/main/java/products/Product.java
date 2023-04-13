7b77f0f03b5896251b15b8e8f29adb2d008205b4
Emmanuel Cajas <emmanuelcajasballesteros@gmail.com>
2023-04-13 09:47:11 +0200

    prueba

--- src/main/java/products/Product.java
+++ src/main/java/products/Product.java
@@ -1,46 +1,47 @@
 package products;
+// emmanuel
+public class Product {
+    private String name;
+    private double price;
+    private int quantity;
 
-import java.util.Objects;
-
-public sealed abstract class Product permits  Flower, Decoration, Tree{
-    protected String name;
-    protected double price;
-
-
-    public Product(String name, double price) {
+    public Product(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
+        this.quantity = quantity;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public double getPrice() {
         return price;
     }
 
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+
+    }
 
     @Override
     public String toString() {
-        return " | Product name: "+this.name+" | Product price: "+this.price;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if(!(o instanceof Product product)) {
-            return false;
-        }
-        else if (this.name.equals(product.name)) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    @Override
-    public int hashCode() {
-        return this.name.hashCode();
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
     }
 }
