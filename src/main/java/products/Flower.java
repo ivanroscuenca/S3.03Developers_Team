3d4edeba92258a8b693c6c6da4a7fc04dc31f091
ivanroscuenca <ivanros@protonmail.com>
2023-04-17 10:17:23 +0200

    numflowers static

--- src/main/java/products/Flower.java
+++ src/main/java/products/Flower.java
@@ -2,6 +2,10 @@ package products;
 
 public class Flower extends Product {
 
+    //ivan
+
+    static int numFlowers;
+
     String colour;
     public Flower(String name, double price, int quantity,String colour) {
         super(name, price, quantity);
