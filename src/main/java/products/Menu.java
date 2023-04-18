3f90b35faff0ac0fdaef4f346d551ac5250af564
unknown <antoni.llombart@gmail.com>
2023-04-18 11:01:36 +0200

    file updaters ok

--- src/main/java/products/Menu.java
+++ src/main/java/products/Menu.java
@@ -1,55 +1,41 @@
 package products;
 
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Menu {
+    public static Scanner sc = new Scanner(System.in);
+    public static Store store = new Store("FlowerWorld");
     public void menu() {
-        Store store = new Store("FlowerWorld");
-        Scanner sc = new Scanner(System.in);
         boolean continueExecution=true;
         int option;
-        System.out.println("Choose and option: ");
-        System.out.println("1: Adding tree");
-        System.out.println("2: Adding flower");
-        System.out.println("3: Adding decoration");
-        System.out.println("4: Remove tree");
-        System.out.println("5: Remove flower");
-        System.out.println("6: Remove decoration");
-        System.out.println("7: Printing stock");
-        System.out.println("8: Printing stock - full stock");
-        System.out.println("9: Printing total stock value");
-        System.out.println("10: Generating purchase ticket");
-        System.out.println("11: Listing purchase tickets");
-        System.out.println("12: Printing purchases incoming");
-
+        menuHeader();
         while(continueExecution) {
             option = Integer.parseInt(sc.nextLine());
             switch (option) {
                 case 1: {
-                    System.out.println("Type tree name");
-                    String name=sc.nextLine();
-                    System.out.println("Type tree price");
-                    Double price=Double.parseDouble(sc.nextLine());
-                    System.out.println("Type trees quantity");
-                    Integer quantity=Integer.parseInt(sc.nextLine());
-                    System.out.println("Type tree height");
-                    Double height=Double.parseDouble(sc.nextLine());
-                    store.addTree(new Tree(name,price,quantity,height));
+                    addTreeMenu();
                     break;
                 }
                 case 2: {
+                    addFlowerMenu();
                     break;
                 }
                 case 3: {
+                    addDecorationMenu();
                     break;
                 }
                 case 4: {
+                    menuTreeRemove();
                     break;
                 }
                 case 5: {
+                    menuFlowerRemove();
                     break;
                 }
                 case 6: {
+                    menuDecorationRemove();
                     break;
                 }
                 case 7: {
@@ -76,6 +62,113 @@ public class Menu {
                 }
             }
         }
+    }
+    public static void menuHeader() {
+        System.out.println("Choose and option: ");
+        System.out.println("1: Adding tree");
+        System.out.println("2: Adding flower");
+        System.out.println("3: Adding decoration");
+        System.out.println("4: Remove tree");
+        System.out.println("5: Remove flower");
+        System.out.println("6: Remove decoration");
+        System.out.println("7: Printing stock");
+        System.out.println("8: Printing stock - full stock");
+        System.out.println("9: Printing total stock value");
+        System.out.println("10: Generating purchase ticket");
+        System.out.println("11: Listing purchase tickets");
+        System.out.println("12: Printing purchases incoming");
+    }
+    public static void addTreeMenu() {
+        System.out.println("Type tree name");
+        String name=sc.nextLine();
+        System.out.println("Type tree price");
+        Double price=Double.parseDouble(sc.nextLine());
+        System.out.println("Type trees quantity");
+        Integer quantity=Integer.parseInt(sc.nextLine());
+        System.out.println("Type tree height");
+        Double height=Double.parseDouble(sc.nextLine());
+        store.addTree(new Tree(name,price,quantity,height));
+    }
 
+    public static void addFlowerMenu() {
+        System.out.println("Type flower name");
+        String name=sc.nextLine();
+        System.out.println("Type flower price");
+        Double price=Double.parseDouble(sc.nextLine());
+        System.out.println("Type flowers quantity");
+        Integer quantity=Integer.parseInt(sc.nextLine());
+        System.out.println("Type flower colour");
+        String colour=sc.nextLine();
+        store.addFlower(new Flower(name,price,quantity,colour));
+    }
+
+    public static void addDecorationMenu() {
+        System.out.println("Type decoration name");
+        String name=sc.nextLine();
+        System.out.println("Type decoration price");
+        Double price=Double.parseDouble(sc.nextLine());
+        System.out.println("Type decoration quantity");
+        Integer quantity=Integer.parseInt(sc.nextLine());
+        System.out.println("Type tree material: wood (W) or plastic (P)");
+        String material=sc.nextLine();
+        Materials auxMaterial=null;
+        if(material.equals("W")) {
+            auxMaterial=Materials.WOOD;
+        }
+        else if(material.equals("P")){
+            auxMaterial=Materials.PLASTIC;
+        }
+        store.addDecoration(new Decoration(name,price,quantity,auxMaterial));
+        Utils.fromDecorationSetToFile(store.getDecorations());
+    }
+    public static void menuTreeRemove() {
+        System.out.println("Which tree do you want to remove? Type its name");
+        printingSet(store.getTrees());
+        String name=sc.nextLine();
+        boolean treeNotFound=true;
+        Iterator it = store.getTrees().iterator();
+        while(treeNotFound && it.hasNext()) {
+            Tree tree = (Tree) it.next();
+            if(tree.name.equals(name)) {
+                it.remove();
+                treeNotFound = false;
+            }
+        }
+        Utils.fromTreeSetToFile(store.getTrees());
+    }
+    public static void menuFlowerRemove() {
+        System.out.println("Which flower do you want to remove? Type its name");
+        printingSet(store.getFlowers());
+        String name=sc.nextLine();
+        boolean flowerNotFound=true;
+        Iterator it = store.getFlowers().iterator();
+        while(flowerNotFound && it.hasNext()) {
+            Flower flower = (Flower) it.next();
+            if(flower.name.equals(name)) {
+                it.remove();
+                flowerNotFound = false;
+            }
+        }
+        Utils.fromFlowerSetToFile(store.getFlowers());
+    }
+    public static void menuDecorationRemove() {
+        System.out.println("Which decoration do you want to remove? Type its name");
+        printingSet(store.getDecorations());
+        String name=sc.nextLine();
+        boolean decorationNotFound=true;
+        Iterator it = store.getFlowers().iterator();
+        while(decorationNotFound && it.hasNext()) {
+            Decoration decoration = (Decoration) it.next();
+            if(decoration.name.equals(name)) {
+                it.remove();
+                decorationNotFound = false;
+            }
+        }
+        Utils.fromDecorationSetToFile(store.getDecorations());
+    }
+    public static void printingSet(Set<? extends Product> set) {
+        for(Product product:set) {
+            System.out.println(product);
+        }
     }
 }
--- src/main/java/products/Store.java
+++ src/main/java/products/Store.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.util.*;
 
 public class Store {
-
     private String nameStore;
     private static Store store;
 
@@ -21,8 +20,6 @@ public class Store {
         trees = new HashSet<Tree>();
         decorations = new HashSet<Decoration>();
     }
-
-
     /* Making Store singleton */
     public static Store getStore(String name) {
         if(store==null) {
@@ -32,6 +29,22 @@ public class Store {
             store.nameStore=name;
             return store;
         }
+    }
+
+    public HashSet<Flower> getFlowers() {
+        return flowers;
+    }
+
+    public HashSet<Tree> getTrees() {
+        return trees;
+    }
+
+    public HashSet<Decoration> getDecorations() {
+        return decorations;
+    }
+
+    public double getStockValue() {
+        return stockValue;
     }
 
     public String Stock() {
@@ -46,54 +59,15 @@ public class Store {
     public void addTree(Tree tree) {
         trees.add(tree);
     }
-
     public void addDecoration(Decoration decoration) {
         decorations.add(decoration);
     }
-
-    public void removeFlower(Flower flower) {
-        flowers.remove(flower);
-    }
-
-    public void removeTree(Tree tree) {
-        trees.remove(tree);
-    }
-
-    public void removeDecoration(Decoration decoration) {
-        decorations.remove(decoration);
-    }
-
 
     public void printStock(){
 
         flowers.stream().forEach(System.out::print);
         trees.stream().forEach(System.out::print);
         decorations.stream().forEach(System.out::print);
-
-
-        /*for (Flower flower: flowers   ) {
-            System.out.println(flower);
-        }
-
-        for (Tree tree:trees   ) {
-            System.out.println(tree);
-        }
-
-        for (Decoration decoration: decorations   ) {
-            System.out.println(decoration);
-        }*/
-
-    }
-
-    @Override
-    public String toString() {
-        return "Store{" +
-                "nameStore='" + nameStore + '\'' +
-                ", flowers=" + flowers +
-                ", trees=" + trees +
-                ", decorations=" + decorations +
-                ", stockValue=" + stockValue +
-                '}';
     }
 
     public void updateTrees(String name, int newQuantity){
@@ -131,60 +105,6 @@ public class Store {
             }
         }
     }
-
-
-/*
-
-
-    public void saveProductsToFile(String fileName) {
-        try {
-            FileWriter fileWriter = new FileWriter(fileName);
-            PrintWriter printWriter = new PrintWriter(fileWriter);
-
-            for (Product product : products) {
-                printWriter.println("Product : " + product.getName() + ", Price: " + product.getPrice() + ".");
-            }
-
-            printWriter.close();
-        } catch (IOException e) {
-            System.err.printf("Error writing the file : ", e.getMessage());
-        }
-
-    }
-
-    public String getTotalQty() {
-
-        String sentence="";
-        for (Product product : products) {
-
-            sentence+= "Product : " + product.getName() + ", Quantity : "+".";
-        }
-        return sentence;
-    }
-
-
-
-    public double getTotalValue() {
-            double totalValue = 0.0;
-
-            for (Product product : products) {
-
-            }
-
-            return totalValue;
-        }
-
-    public Product getProductByName(String name) {
-        for (Product product : products) {
-            if (product.getName().equals(name)) {
-                return product;
-            }
-        }
-        return null;
-    }
-
- */
-
 }
 
 
